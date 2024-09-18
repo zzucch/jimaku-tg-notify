f398f72c932814fab7d5759b5852b7bb5482cd0d
zzucch <nikitamilovatskiy@gmail.com>
2024-09-18 04:52:18 +0300

    feat: add handling too long messages

--- internal/bot/message.go
+++ internal/bot/message.go
@@ -10,15 +10,35 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-const maxRetries = 5
+const (
+	maxRetries       = 5
+	maxMessageLength = 4096
+)
 
 func (b *Bot) SendMessage(chatID int64, text string) error {
+	if len(text) > maxMessageLength {
+		chunks := splitMessage(text, maxMessageLength)
+
+		for _, chunk := range chunks {
+			if err := b.sendMessageChunk(chatID, chunk); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	return b.sendMessageChunk(chatID, text)
+}
+
+func (b *Bot) sendMessageChunk(chatID int64, text string) error {
 	message := tgbotapi.NewMessage(chatID, text)
 
 	for retry := 0; retry < maxRetries; retry++ {
 		if _, err := b.botAPI.Send(message); err != nil {
 			if retryAfter, ok := getRetryAfterDuration(err); ok {
 				retryAfter *= 2
+
 				log.Warn("too many requests to telegram", "retry after", retryAfter)
 				time.Sleep(retryAfter)
 
@@ -34,6 +54,23 @@ func (b *Bot) SendMessage(chatID int64, text string) error {
 	log.Error("failed to send message", "max retries", maxRetries)
 
 	return fmt.Errorf("failed to send message after %d retries", maxRetries)
+}
+
+func splitMessage(text string, maxLength int) []string {
+	var chunks []string
+
+	for len(text) > maxLength {
+		lastSpaceIndex := strings.LastIndex(text[:maxLength], " ")
+		if lastSpaceIndex == -1 {
+			lastSpaceIndex = maxLength
+		}
+
+		chunks = append(chunks, text[:lastSpaceIndex])
+		text = text[lastSpaceIndex:]
+	}
+
+	chunks = append(chunks, text)
+	return chunks
 }
 
 func getRetryAfterDuration(err error) (time.Duration, bool) {
